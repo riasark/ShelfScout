0e1f0cc7b94c16bfe5ddea02fc16751f3b30f9c3
ria sarkar <jitasark@gmail.com>
2024-05-19 10:32:22 -0400

    connection with database established

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3030;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
